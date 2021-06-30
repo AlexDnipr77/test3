517c5f2dfdb32ba2ae73fadf11795911f2b0749e
AlexDnipr77 <alexdnipr@gmail.com>
2021-06-30 16:39:59 +0300

    c2

--- ui/build/main.js
+++ ui/build/main.js
@@ -1,1 +1,18 @@
 console.log('hello world');
+
+console.log(window.location);
+
+setTimeout(() => {
+    console.log('timeout');
+    // window.location.href = "/another"
+
+
+    fetch('/another').then(async x => {
+        console.log('fetched-----------------------------------------------------------');
+        console.log(x);
+        // console.log(x.text())
+
+        let text = await x.text();
+        console.log(text);
+    })
+}, 4000)
